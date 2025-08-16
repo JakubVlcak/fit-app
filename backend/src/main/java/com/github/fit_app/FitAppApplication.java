1f36718da02aa8dd5153e9c0e9f9f7bc2ad349f9
mvlcak <72494996+mvlcak@users.noreply.github.com>
2025-08-16 15:45:43 +0200

    add backend spring boot starter

---
+++ backend/src/main/java/com/github/fit_app/FitAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.fit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/fit_app/FitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
